122096de98a493fd9ff5e1b60eaafaea93db055b
friend1019 <genie1019002@gmail.com>
2025-04-14 19:44:49 +0900

    food쪽 수정

--- src/jsx/Food.jsx
+++ src/jsx/Food.jsx
@@ -1,12 +1,15 @@
 import truckImg from "../img/푸드트럭.png";
+import "../css/Food.css";
+import 지도 from "../img/지도.png";
 
 const Food = () => {
   return (
     <div className="food-container">
         <h2>푸드트럭의 정보를 확인하세요</h2>
-      <div className="truck-img">
-        <img src={truckImg} alt="푸드트럭" />
-      </div>
+        <img src={지도} alt="map-img" className="map-img" />
+        <div className="truck-img">
+          <img src={truckImg} alt="푸드트럭" />
+        </div>
     </div>
   );
 };
